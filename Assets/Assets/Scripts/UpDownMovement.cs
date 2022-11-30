9ad6dde115276961c89b85a36a5735534467adaf
Sujoy Sinha <900sujoysinha@gmail.com>
2022-11-30 13:43:30 +0530

    now the both player work and some improvement

--- Assets/Assets/Scripts/UpDownMovement.cs
+++ Assets/Assets/Scripts/UpDownMovement.cs
@@ -1,42 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class UpDownMovement2 : MonoBehaviour
+public class UpDownMovement : MonoBehaviour
 {
-    [SerializeField] private float updownspeed1;
+    [SerializeField] private float updownspeed1,updownspeed2;
 
-    private Rigidbody2D rb1;
-    private BoxCollider2D bc1;
+    private Rigidbody2D rb1,rb2;
+    private BoxCollider2D bc1,bc2;
 
-    public PhotonView pv1;
+    public PhotonView pv1,pv2;
 
     private void Awake1()
     {
         rb1 = GetComponent<Rigidbody2D>();
         bc1 = GetComponent<BoxCollider2D>();
+
+        rb2 = GetComponent<Rigidbody2D>();
+        bc2 = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
     {
-        CheckInput1();
         /*if(pv.isMine)
         {
             CheckInput();
         }*/
+        if(gameObject.tag == "P1")
+        {
+            CheckInput1();
+        }
+        else if(gameObject.tag == "P2/AI")
+        {
+            CheckInput2();
+        }
     }
 
     private void CheckInput1()
     {
-        Vector3 posY = transform.position;
+        Vector3 posY1 = transform.position;
         if (Input.GetKey(KeyCode.W))
         {
-            posY.y += updownspeed1 * Time.deltaTime;
+            posY1.y += updownspeed1 * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            posY.y -= updownspeed1 * Time.deltaTime;
+            posY1.y -= updownspeed1 * Time.deltaTime;
         }
-        transform.position = posY;
+        transform.position = posY1;
+    }
+    private void CheckInput2()
+    {
+        Vector3 posY2 = transform.position;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            posY2.y += updownspeed2 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            posY2.y -= updownspeed2 * Time.deltaTime;
+        }
+        transform.position = posY2;
     }
 }
